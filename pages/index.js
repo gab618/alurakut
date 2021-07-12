735ecba3afdd920ec60dc920429787d7616a4996
Gabriel <knuckles4x@gmail.com>
2021-07-12 20:09:35 -0300

    ProfileSidebar

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,25 @@
 import { MainGrid } from "../src/components/MainGrid";
 import { Box } from "../src/components/Box";
 
+function ProfileSidebar({ githubUser }) {
+  return (
+    <Box>
+      <img
+        src={`https://github.com/${githubUser}.png`}
+        alt={githubUser}
+        style={{ borderRadius: "8px" }}
+      />
+    </Box>
+  );
+}
+
 export default function Home() {
+  const githubUser = "gab618";
+
   return (
     <MainGrid>
       <div className="profileArea" style={{ gridArea: "profileArea" }}>
-        <Box>
-          <img
-            src="https://pbs.twimg.com/profile_images/1399458086503464967/bOD-QW2K_400x400.png"
-            alt="avatar"
-            style={{ borderRadius: "8px" }}
-          />
-        </Box>
+        <ProfileSidebar githubUser={githubUser} />
       </div>
       <div style={{ gridArea: "welcomeArea" }}>
         <Box>Bem vindo</Box>
