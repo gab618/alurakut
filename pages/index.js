9cf49a592dc32d78903c015310d5199e13b86929
Gabriel <knuckles4x@gmail.com>
2021-07-12 12:51:53 -0300

    main components

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,24 @@
-import styled from "styled-components";
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`;
-const Box = styled.div`
-  background: #ffffff;
-  border-radius: 8px;
-`;
+import { MainGrid } from "../src/components/MainGrid";
+import { Box } from "../src/components/Box";
 
 export default function Home() {
   return (
-    <main>
-      <Box>Imagem</Box>
-      <Box>Bem vindo</Box>
-    </main>
+    <MainGrid>
+      <div className="profileArea" style={{ gridArea: "profileArea" }}>
+        <Box>
+          <img
+            src="https://pbs.twimg.com/profile_images/1399458086503464967/bOD-QW2K_400x400.png"
+            alt="avatar"
+            style={{ borderRadius: "8px" }}
+          />
+        </Box>
+      </div>
+      <div style={{ gridArea: "welcomeArea" }}>
+        <Box>Bem vindo</Box>
+      </div>
+      <div style={{ gridArea: "profileRelationsArea" }}>
+        <Box>Heróis</Box>
+      </div>
+    </MainGrid>
   );
 }
